b450c4b6f1959fdc1ff3ef906a9b7ee6e1581150
342173696 <342173696@qq.com>
2017-10-21 16:35:26 +0800

    pass calculator3

--- calculator3.py
+++ calculator3.py
@@ -80,40 +80,40 @@ class Userdata(Config):
                     insurance = self._config['JiShuH'] * insurance_per
                 else:
                     insurance = salary * insurance_per
-                    ratal = salary - insurance - 3500
+                ratal = salary - insurance - 3500
 
-                    if ratal <= 1500:
-                        tax_rate = 0.03
-                        quick_deduction = 0
-                    elif ratal <= 4500:
-                        tax_rate = 0.1
-                        quick_deduction = 105
-                    elif ratal <= 9000:
-                        tax_rate = 0.2
-                        quick_deduction = 555
-                    elif ratal <= 35000:
-                        tax_rate = 0.25
-                        quick_deduction = 1005
-                    elif ratal <= 55000:
-                        tax_rate = 0.3
-                        quick_deduction = 5505
-                    elif ratal <= 80000:
-                        tax_rate = 0.35
-                        quick_deduction = 5505
-                    else:
-                        tax_rate = 0.45
-                        quick_deduction = 13505
+                if ratal <= 1500:
+                    tax_rate = 0.03
+                    quick_deduction = 0
+                elif ratal <= 4500:
+                    tax_rate = 0.1
+                    quick_deduction = 105
+                elif ratal <= 9000:
+                    tax_rate = 0.2
+                    quick_deduction = 555
+                elif ratal <= 35000:
+                    tax_rate = 0.25
+                    quick_deduction = 1005
+                elif ratal <= 55000:
+                    tax_rate = 0.3
+                    quick_deduction = 5505
+                elif ratal <= 80000:
+                    tax_rate = 0.35
+                    quick_deduction = 5505
+                else:
+                    tax_rate = 0.45
+                    quick_deduction = 13505
 
-                    if ratal < 0:
-                        tax = 0 
-                    else:
-                        tax = ratal * tax_rate - quick_deduction
-                    income = salary - insurance - tax
-                                   
-                    self.result.append(
-                                       "{},{},{:.2f},{:.2f},{:0.2f}".format(
-                                       employee_id,salary,insurance,tax,income)
-                                      )
+                if ratal < 0:
+                    tax = 0 
+                else:
+                    tax = ratal * tax_rate - quick_deduction
+                income = salary - insurance - tax
+                               
+                self.result.append(
+                                   "{},{},{:.2f},{:.2f},{:0.2f}".format(
+                                   employee_id,salary,insurance,tax,income)
+                                  )
             return self.result 
                # print(self.result)
         except:
