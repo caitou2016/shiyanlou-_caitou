29ad9bc0052dade8b509d89fbfa84ace5e49b3a5
342173696 <342173696@qq.com>
2017-10-27 14:28:24 +0800

    finish lab2

--- lab2/news/app.py
+++ lab2/news/app.py
@@ -9,17 +9,30 @@ app = Flask(__name__)
 #app.run(port=3000)
 
 app.config['TEMPLATES_AUTO_RELOAD']= True
-files_path = '/home/shiyanlou/files'
+dir_path = '/home/shiyanlou/files'
 #file_list = []
 @app.route('/')
 def index():
     file_list = []
     # file_name = 'helloshiyanlou.json'
-    files = os.listdir(files_path)
+    files = os.listdir(dir_path)
     for news_file in files:
-        file_name = os.path.join(files_path,news_file)
+        file_name = os.path.join(dir_path,news_file)
         with open(file_name,'r') as file_read:
-            file_list.append(json.loads(file_read.read()))
+            file_list.append(json.loads(file_read.read())['title'])
     return render_template('index.html',files=file_list)
 
+@app.route('/files/<filename>')
+def file(filename):
+    file_path = os.path.join(dir_path,filename+'.json')
+    if os.path.exists(file_path):
+        with open(file_path,'r') as file_read2:
+            file_connect = json.loads(file_read2.read())
+        return render_template('file.html',file_connect=file_connect)
+    else:
+        return render_template('404.html')
+#app.run(port=3000)
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html')
